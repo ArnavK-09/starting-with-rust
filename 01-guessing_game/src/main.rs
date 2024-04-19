f9ec34385ae2912a7b91cad2a57ec5b05479b7f6
Arnav K <arnavkaushal09@gmail.com>
2024-04-19 18:13:38 +0000

    guess game

--- 01-guessing_game/src/main.rs
+++ 01-guessing_game/src/main.rs
@@ -1,5 +1,7 @@
 // standard i/o
-use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io; // g;. The Rng trait defines methods that random number generators implement
 
 fn main() {
     // welcome msg
@@ -9,14 +11,39 @@ fn main() {
     █▄█ █▄█ ██▄ ▄█ ▄█   █▄█ █▀█ █░▀░█ ██▄
     "
     );
-    println!("Guess the number:- ");
 
-    // variable to store user guess || new is an associated function of the String type.
-    let mut guess_by_user = String::new(); // empty string
+    loop {
+        println!("\n\nGuess the number:- ");
 
-    // ask user for a number
-    io::stdin()
-        .read_line(&mut guess_by_user)
-        .expect("No number found");
-    println!("\nYou guessed number: {}", guess_by_user);
+        // variable to store user guess || new is an associated function of the String type.
+        let mut guess_by_user = String::new(); // empty string
+
+        // ask user for a number
+        io::stdin()
+            .read_line(&mut guess_by_user)
+            .expect("No number found");
+
+        // parsing string to number 
+        let guess_by_user: u32 = match guess_by_user.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+        };
+
+        // making target number
+        let target_number = rand::thread_rng().gen_range(1..=101);
+        println!("\nYour Target numberwas: {}", target_number);
+
+        // matching values
+        match guess_by_user.cmp(&target_number) {
+            Ordering::Less => println!("Too small!\n"),
+            Ordering::Equal => {
+                println!("POG! You guessed it!\n");
+                break;
+            }
+            Ordering::Greater => println!("Too big!\n"),
+        }
+    }
 }
