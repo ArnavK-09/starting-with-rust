4fb3e734270cb7cf7dead84b8ef81edab25e34c2
Arnav K <arnavkaushal09@gmail.com>
2024-04-20 18:22:25 +0000

    05 comments

---
+++ 05-comments/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!"); // code line comment
+
+    // Simple comment
+
+    // multiple
+    // single line
+    // comment
+
+    // Rust also has another kind of comment, documentation comments, which we’ll discuss in the “Publishing a Crate to Crates.io”
+}
